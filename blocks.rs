2fe3d1514063a26b564c817e4f893b5103eec0e9
Aydar <contact@fetchdev.host>
2020-05-26 16:40:45 +0300

    Removed unn

--- blocks.rs
+++ blocks.rs
@@ -13,9 +13,6 @@ pub fn set_blocks(rblocks: [u8;64], gblocks: [u8;64], bblocks: [u8;64]){
 		let val : usize;
 		if x > 50 && x < 450 && y > 50 && y < 450 {
 			val = (((x / 50) + (8 * ((y / 50) - 1))) - 1) as usize; 
-			// if val != recorded{
-			// 	println!("{}", val); recorded = val; 
-			// }
 			*pixel = image::Rgb([rblocks[val], gblocks[val], bblocks[val]]);
 		}else{
 			*pixel = image::Rgb([255, 255, 255]);
